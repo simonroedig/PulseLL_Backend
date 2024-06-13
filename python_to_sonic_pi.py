e975e8f7c267688752451dca27ec6b94e0113f64
simonroedig <simon.roedig@web.de>
2024-06-13 15:25:34 +0200

    sending sonic pi code from python works, started with audiostream

---
+++ python_to_sonic_pi.py
@@ -0,0 +1,25 @@
+from pythonosc import udp_client
+
+
+"""
+In Sonic-Pi one must run the following code first:
+
+use_osc "localhost", 4560
+
+live_loop :executor do
+  use_real_time
+  code = sync "/osc*/execute_code"
+  eval(code[0])
+end
+"""
+
+file_path = r"current_sonic_pi_code.txt"
+with open(file_path, 'r') as file:
+    sonic_pi_code = file.read()
+    print(file)
+    
+ip = "127.0.0.1"
+port = 4560
+
+client = udp_client.SimpleUDPClient(ip, port)
+client.send_message("/execute_code", [sonic_pi_code])
